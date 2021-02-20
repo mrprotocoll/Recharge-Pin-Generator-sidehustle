ff2be029879a0a2365a6d7bac4b9547b9a7100e5
mrprotocoll <43586022+mrprotocoll@users.noreply.github.com>
2021-02-20 13:55:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$pins = [];
+$x = 1;
+while($x < 251){
+    $randOne = strval(rand(222222,9999999));
+    $randTwo = strval(rand(100000,99999999));
+    $pin = $randOne.$randTwo;
+    if(!in_array($pin,$pins)){
+        $pins[] = $pin;
+        echo $x.") &nbsp;&nbsp;&nbsp;".$pin."<br>";
+        $x++;
+    }
+    
+}
+
+?>
